b1e0b7cb7f64f6e762249f3836640979d0a8f078
Leele-lee <1623450919@qq.com>
2023-01-30 10:49:09 +0800

    Commit LeapYear for the first time.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,14 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author leele
  */
 public class LeapYear {
+    /** Determinate if the given year is LeapYear.
+     * @param year to be analysed
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 4 != 0);
+    }
+         
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
